283931a1afc9851383f86133f93f2b06a27ae59a
Walid Shaari <shaawx0a@aramco.com>
2014-11-16 17:06:47 +0300

    corrections for an extra }

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class puppet_infra::params {
 
-    $hiera_base = "${::settings::confdir}/hieradata}"
+    $hiera_base = "${::settings::confdir}/hieradata"
     $hiera_backends = ['yaml']
     $hiera_hierarchy = ['"%{clientcert}"','global']
     $hiera_template = "${module_name}/hiera.yaml.erb"
