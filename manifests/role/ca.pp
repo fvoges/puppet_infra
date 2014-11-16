2de1ba2d8a6dd18dd108cea54b56621561178c22
Walid Shaari <shaawx0a@aramco.com>
2014-11-16 16:22:13 +0300

    correcting role/{ca.pp,master.pp} to reflect earlier change in console

--- manifests/role/ca.pp
+++ manifests/role/ca.pp
@@ -2,6 +2,4 @@ class puppet_infra::role::ca {
   include puppet_infra::profile::master
   include puppet_infra::profile::ca
   include puppet_infra::profile::mcollective
-  include puppet_infra::profile::noconsole
-  include puppet_infra::profile::no_node_terminus
 }
--- manifests/role/master.pp
+++ manifests/role/master.pp
@@ -1,6 +1,4 @@
 class puppet_infra::role::ca {
   include puppet_infra::profile::master
   include puppet_infra::profile::mcollective
-  include puppet_infra::profile::noconsole
-  include puppet_infra::profile::no_node_terminus
 }
