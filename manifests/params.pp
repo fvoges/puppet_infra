8ff0eea42b838bbb263f00a23970f1d076308df9
Federico Voges <federico@puppetlabs.com>
2014-11-19 08:36:28 +0000

    added missing  to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,4 +5,5 @@ class puppet_infra::params {
   $hiera_base       = "${::settings::confdir}/hieradata"
   $hiera_backends   = ['yaml']
   $hiera_hierarchy  = ['"%{clientcert}"','global',]
+  $hiera_template   = "${module_name}/hiera.yaml.erb"
 }
