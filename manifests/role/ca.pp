73c0a49996a688a0dd77a93574a6da316f78781e
Walid Shaari <shaawx0a@exchange.aramco.com.sa>
2014-12-11 15:37:25 +0300

    added ganglia profiles to masters, and CA

--- manifests/role/ca.pp
+++ manifests/role/ca.pp
@@ -3,4 +3,7 @@ class puppet_infra::role::ca {
   include puppet_infra::profile::ca
   include puppet_enterprise::profile::amq::hub
   include puppet_infra::profile::mcollective
+  include profiles::monitoring::ganglia::gmond
+  include profiles::monitoring::ganglia::report
+
 }
--- manifests/role/master.pp
+++ manifests/role/master.pp
@@ -2,4 +2,7 @@ class puppet_infra::role::master {
   include puppet_infra::profile::master
   include puppet_enterprise::profile::amq::broker
   include puppet_infra::profile::mcollective
+  include profiles::monitoring::ganglia::gmond
+  include profiles::monitoring::ganglia::report
+
 }
