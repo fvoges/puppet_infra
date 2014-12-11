b9cf0404c75de978169a472dea1366a247688884
Federico Voges <federico@puppetlabs.com>
2014-12-11 11:40:10 +0300

    Added some system tuning and custom console settings for PE

---
+++ manifests/amq/limits.pp
@@ -0,0 +1,13 @@
+# Class: puppet_infra::amq::limits
+#
+#
+class puppet_infra::amq::limits {
+  file { '/etc/security/limits.d/pe-activemq.conf':
+    ensure => file,
+    source => 'puppet:///modules/pe-aqtivemq-limits.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    notify => Service['pe-activemq'],
+  }
+}
--- manifests/profile/console.pp
+++ manifests/profile/console.pp
@@ -2,7 +2,14 @@
 #
 #
 class puppet_infra::profile::console inherits puppet_infra::profile::global {
-  $prune_upto = hiera('puppet_infra::profile::console::prune_upto')
+  $prune_upto                = hiera('puppet_infra::profile::console::prune_upto')
+  $password_reset_expiration = hiera('puppet_infra::profile::console::password_reset_expiration')
+  $session_timeout           = hiera('puppet_infra::profile::console::session_timeout')
+  $failed_attempts_lockout   = hiera('puppet_infra::profile::console::failed_attempts_lockout')
+
+  validate_re($password_reset_expiration, '^\d+$')
+  validate_re($session_timeout, '^\d+$')
+  validate_re($failed_attempts_lockout, '^\d+$')
 
   include puppet_enterprise::license
   include puppet_enterprise::profile::console
@@ -10,5 +17,14 @@ class puppet_infra::profile::console inherits puppet_infra::profile::global {
 
   class { 'pe_console_prune':
     prune_upto => $prune_upto,
+  }
+
+  file { "/etc/puppetlabs/console-services/conf.d/custom-settings.conf":
+    ensure  => file,
+    source  => 'puppet:///modules/console-settings.conf.erb',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    notify => Service['pe-console-services'],
   }
 }
