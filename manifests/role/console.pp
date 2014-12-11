5fba3f4fcb3667aa46ee56482a3ee0c265b423ab
Walid Shaari <shaawx0a@exchange.aramco.com.sa>
2014-12-11 18:05:54 +0300

    added profile monitoring gmond to console, and puppetdb

--- manifests/role/console.pp
+++ manifests/role/console.pp
@@ -1,4 +1,6 @@
 class puppet_infra::role::console {
   include puppet_infra::profile::console
   include puppet_infra::profile::mcollective
+  include profiles::monitoring::ganglia::gmond
+
 }
--- manifests/role/puppetdb.pp
+++ manifests/role/puppetdb.pp
@@ -1,3 +1,5 @@
 class puppet_infra::role::puppetdb {
   include puppet_infra::profile::puppetdb
+  include profiles::monitoring::ganglia::gmond
+
 }
