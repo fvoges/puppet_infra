8242986266c67ebe9506364bd1ec3c0b38cf82e4
Walid Shaari <shaawx0a@exchange.aramco.com.sa>
2014-12-27 17:47:55 +0300

    linting master, and CA as well adding scoping

--- manifests/role/ca.pp
+++ manifests/role/ca.pp
@@ -1,9 +1,10 @@
+# class puppet_infra::role::ca
 class puppet_infra::role::ca {
-  include puppet_infra::profile::master
-  include puppet_infra::profile::ca
-  include ::puppet_infra::profile::hub 
-  include puppet_infra::profile::mcollective
-  include profiles::monitoring::ganglia::gmond
-  include profiles::monitoring::ganglia::report
+  include ::puppet_infra::profile::master
+  include ::puppet_infra::profile::ca
+  include ::puppet_infra::profile::hub
+  include ::puppet_infra::profile::mcollective
+  include ::profiles::monitoring::ganglia::gmond
+  include ::profiles::monitoring::ganglia::report
 
 }
--- manifests/role/master.pp
+++ manifests/role/master.pp
@@ -1,8 +1,9 @@
+# class puppet_infra::role::master
 class puppet_infra::role::master {
-  include puppet_infra::profile::master
-  include puppet_infra::profile::broker
-  include puppet_infra::profile::mcollective
-  include profiles::monitoring::ganglia::gmond
-  include profiles::monitoring::ganglia::report
+  include ::puppet_infra::profile::master
+  include ::puppet_infra::profile::broker
+  include ::puppet_infra::profile::mcollective
+  include ::profiles::monitoring::ganglia::gmond
+  include ::profiles::monitoring::ganglia::report
 
 }
