70d4cf625bc709fb89edc348daec4a135dda209c
Walid Shaari <shaawx0a@exchange.aramco.com.sa>
2014-12-27 16:31:17 +0300

    refactored amq limits into hub, and broker

--- manifests/profile/broker.pp
+++ manifests/profile/broker.pp
@@ -4,13 +4,5 @@
 class puppet_infra::profile::broker inherits puppet_infra::profile::global {
   include ::puppet_enterprise::profile::amq::broker
   include ::puppet_infra::amq::logrotate
-
-  file { '/etc/security/limits.d/10-pe-activemq.conf':
-    ensure => file,
-    source => "puppet:///modules/${module_name}/pe-activemq-limits.conf",
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    notify => Service['pe-activemq'],
-  }
+  include ::puppet_infra::amq::limits
 }
--- manifests/profile/hub.pp
+++ manifests/profile/hub.pp
@@ -4,13 +4,5 @@
 class puppet_infra::profile::hub inherits puppet_infra::profile::global {
   include ::puppet_enterprise::profile::amq::hub
   include ::puppet_infra::amq::logrotate
-
-  file { '/etc/security/limits.d/10-pe-activemq.conf':
-    ensure => file,
-    source => "puppet:///modules/${module_name}/pe-activemq-limits.conf",
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    notify => Service['pe-activemq'],
-  }
+  include ::puppet_infra::amq::limits
 }
