998e6d93af46edf91526ccc6d7787a53ddf7d38a
Walid Shaari <shaawx0a@exchange.aramco.com.sa>
2014-12-27 17:00:42 +0300

    fixed reference to broker & hub to be infra not enterpise

--- manifests/role/ca.pp
+++ manifests/role/ca.pp
@@ -1,7 +1,7 @@
 class puppet_infra::role::ca {
   include puppet_infra::profile::master
   include puppet_infra::profile::ca
-  include puppet_enterprise::profile::amq::hub
+  include ::puppet_infra::profile::hub 
   include puppet_infra::profile::mcollective
   include profiles::monitoring::ganglia::gmond
   include profiles::monitoring::ganglia::report
--- manifests/role/master.pp
+++ manifests/role/master.pp
@@ -1,6 +1,6 @@
 class puppet_infra::role::master {
   include puppet_infra::profile::master
-  include puppet_enterprise::profile::amq::broker
+  include puppet_infra::profile::broker
   include puppet_infra::profile::mcollective
   include profiles::monitoring::ganglia::gmond
   include profiles::monitoring::ganglia::report
