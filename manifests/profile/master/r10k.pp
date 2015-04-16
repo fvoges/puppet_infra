ae163113a722e59d1e97d9a89930c4ff16e5e5b2
Federico Voges <federico@puppetlabs.com>
2015-04-16 14:21:18 +0200

    Removed old code no longer needed for current version of r10k

--- manifests/profile/master/r10k.pp
+++ manifests/profile/master/r10k.pp
@@ -33,24 +33,13 @@ class puppet_infra::profile::master::r10k {
         'prefix'  => false,
       }
     },
-    purgedirs         => $environmentpath,
-    manage_modulepath => false,
     install_options   => $install_options,
-  }
-
-  # Remove the default production folder so git/r10k does not complain
-  # This will then be regenerated from the this repos production branch
-  exec {'rm -rf production':
-    creates => "${environmentpath}/production/Puppetfile",
-    command => "rm -rf ${environmentpath}/production",
-    require => Class['::r10k'],
-    path    => $::path,
   }
 
   exec {'r10k deploy environment -p':
     creates => "${environmentpath}/production/Puppetfile",
     command => '/opt/puppet/bin/r10k deploy environment -p',
-    require => Exec['rm -rf production'],
+    require => Class['r10k'],
   }
 
 
